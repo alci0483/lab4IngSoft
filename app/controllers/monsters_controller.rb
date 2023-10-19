9eef3dd3c94683172946d2fc8910b817577f6213
alci0483 <alci0483@gmail.com>
2023-10-19 15:27:38 -0300

    termine el labs

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -5,5 +5,17 @@ class MonstersController < ApplicationController
 
   def show
     @monster = Monster.find(params[:id])
+
+    if @monster.tweets.empty?
+      # Si el monstruo no tiene tweets, redirige a la vista de creación de tweets
+      redirect_to tweets_new_path(monster_id: @monster.id)
+    end
   end
+
+  def destroy
+    @monster = Monster.find(params[:id])
+    @monster.destroy
+    redirect_to monsters_path, notice: "Monstruo eliminado exitosamente"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   resources :monsters, only: [:index, :show]
+  resources :tweets, only: [:index, :show, :new, :create]
 
-  root "monsters#index"
+
+  get '/tweets', to: 'tweets#index'
+  get '/tweets/new', to: 'tweets#new'
+  post '/tweets', to: 'tweets#create'
+  get '/tweets/:id', to: 'tweets#show'
+  get '/tweets/:id/edit', to: 'tweets#edit'
+  put '/tweets/:id', to: 'tweets#update'
+  patch '/tweets/:id', to: 'tweets#update'
+  delete '/tweets/:id', to: 'tweets#destroy'
+
+  root "tweets#index"
+
 end
